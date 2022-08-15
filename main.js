80ded3e6f63d39669a22d3e391a1ca097c07f553
davidcufre1997 <107009875+davidcufre1997@users.noreply.github.com>
2022-08-15 18:15:23 -0300

    se sube el desafio

---
+++ main.js
@@ -0,0 +1,14 @@
+function verificar()
+
+var contraseña= document.getElementById("contra").value
+contraseña=parseInt("contraseña")
+
+var confirmar= document.getElementById("confirm").value
+confirmar=parseInt("confirm")
+
+if (contraseña==confirmar){
+alert("la contraseña es valida")}
+
+
+
+
